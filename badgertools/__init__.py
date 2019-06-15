4bfb31c313f988ef0580a4bd6c4114811da03aed
djmcnay <djmcnay@gmail.com>
2019-06-15 17:19:48 +0100

    Initial commit

---
+++ badgertools/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 15 16:56:31 2019
+
+@author: David
+"""
+
---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: D J McNay
+Created on Sat Jun 15 - loitering at Blackbird Bakery in East Dulwich
+
+"""
+
+from setuptools import setup
+
+setup(
+    name = 'badgertools',
+    version = '0.1dev',
+    packages=['badgertools'],
+    install_requires=['plotly'],
+    author = 'David J McNay',
+    author_email = 'djmcnay@gmail.com',
+    license = 'MIT',
+    description='test packaging',
+    long_description=open('README.txt').read(),
+)
