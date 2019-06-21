e53c37c62dbb9c7632d3e9a8828fca315d0ad64f
djmcnay <djmcnay@gmail.com>
2019-06-21 17:07:50 +0100

    Update pokemon.py
    Updating comments and adding Plot function

--- hongxiongmao/pokemon.py
+++ hongxiongmao/pokemon.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+POKEMON Fair Value Bond Model
+    
+Model estimates Fair Value or a long (10-year) bond using multi-variate
+regression. We assume the long term yield can be expressed as a combination
+of the short term interest rate, inflation and growth expectations.
 
+    FV = Intercept + (B1 * short_rate) + (B2 * inflation)
+                   + (B3 * growth) + (B4 * global factor)
+
+Where possible we want the model to be forward looking:
+    
+    interest rate - proxied by the 2-year nominal where possible
+    inflation - ideally swap or survey based i.e. UMich or 5y5y fwd
+    growth - PMI, Tankan or similar
+
+Default here is to download data from Quandl for a specified region and run
+both a full_sample & a rolling regression. The model metrics are stored in 
+dictionaries is the "Default Data & Attributes" cell. These aren't necessarily
+the unconstrained variables I would choose, but are the best compromise I 
+can find whilst limiting to Quandl datasets.
+
+CLASS MODULES:
+    pokemon - acts on a single region or set of data
+    pokemon_go - combines multiple regions
 """
 
+import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
@@ -13,6 +37,23 @@ from hongxiongmao.utilities import tools, download
 # %%
 
 class pokemon(object):
+    """
+    Pokemon
+    
+    For more complete information on the model read the help on the Class Module.
+    This class operates on a single region (or set of input data). 
+    
+    ATTRIBUTES:
+        self.data = model input data; can be input manually or via dl function
+        self.stats_full = full period statistics (pretty useless)
+        self.stats_roll = rolling window statistics
+        self.pokemodel = DataFrame with model output
+    
+    FUNCTIONS:
+        download_and_run()
+        pull_data_from_quandl()
+        model()
+    """
     
     def __init__(self, region='US'):
         self.region = region
@@ -157,19 +198,16 @@ class pokemon_go(object):
     Aggregation class for Pokemon model, will run several Pokemon models at once
     and store the data internally. Option also available to generate Plotly plot.
     Name is a nod to the iPhone Game Will & I were playing during first build.
-    
     """
     
-    def __init__(self, regions=['US']):
-        
+    def __init__(self, regions=['US', 'UK', 'DR', 'JP']):
         self.regions = regions
         self.data, self.stats, self.pokemodel = [dict.fromkeys(regions)] * 3
-        #self.stats = dict.fromkeys(regions)
-        # self.pokemodel = dict.fromkeys(regions)
 
     def download_and_run(self, output=False):
         """
-        
+        Iterate through self.regions, download Quandl data & run Pokemon model
+        Store all data in self.data, self.stats & self.pokemodel
         """
         
         for r in self.regions:        # iterate through each region
@@ -184,11 +222,84 @@ class pokemon_go(object):
         if output:
             return self.pokemodel, self.stats, self.data
         
-    def plotlyplot():
+    def plotlyplot(self, dx=None, output=False):
+        """
+        Plotly Plot for Pokemon Model
+        Two panes with Absolute numbers at the top & Relatives below
+        Buttons to select between individual Regions/Models
         
-        return fig
+        OUTPUT:
+            Plotly dictionary of form dict(data=data, layout=layout)
+            Still needs to be converted into PLotly Graph Object externally
+        
+        NB/ Needs to be updated when High Level Plotly thing developed
+        """
+                
+        # Data - Setup as dictionary of Dataframes
+        dx = self.pokemodel if dx == None else dx
+        
+        # Colourmap for charts
+        cmap = {0:'purple', 1:'turquoise', 2:'grey', 3:'black', 4:'lime', 5:'blue', 6:'orange', 7:'red'}
+        linewidth = [1]
+        
+        # Calc Abs(Max) relative from All pokemons - to use as range for yaxis2
+        mxFunc = lambda r, i: np.max(abs(dx[r].iloc[:,2]-dx[r].iloc[:,i]))
+        mx = np.max([np.max([mxFunc(r,0), mxFunc(r,1)]) for r in dx])
+        
+        # Plotly Layout Dictionary
+        layout = dict(title= 'Pokemon Fair Value Bond Model',
+                      font = {'family':'Courier New', 'size':12},
+                      height = 500,
+                      margin = {'l':100, 'r':50, 'b':50, 't':50},
+                      legend = {'orientation':'h'},
+                      xaxis1= {'domain': [0, 1], 'anchor': 'y1', 'title': '',},
+                      xaxis2= {'domain': [0, 1], 'anchor': 'y2', 'title': '',},
+                      yaxis1= {'domain': [0.42, 1.0], 'anchor': 'x1', 'title': '',
+                               'hoverformat':'.2f', 'tickformat':'.1f'},
+                      yaxis2= {'domain': [0.0, 0.38], 'anchor': 'x2', 'title': 'relative', 'dtick':0.5,
+                               'hoverformat':'.2f', 'tickformat':'.1f', 'range':[-mx, +mx]}, 
+                      updatemenus= [dict(type='buttons', active=-1, showactive = True,
+                                         direction='down', pad = {'l':0, 'r':35, 't':0, 'b':0},
+                                         y=1, x=0,
+                                         buttons=[])],
+                     annotations=[])
+    
+        # Plotly Data
+        data = []
+        for j, r in enumerate(dx.keys()):
             
-# %%
-
-p = pokemon_go().download_and_run(True)
-
+            # Top plot - Absolute
+            for i, n in enumerate(['full_sample', 'rolling', 'long_bond']):
+                data.append(dict(type='scatter', name=n, xaxis='x1', yaxis='y1',
+                                 x=dx[r].index,
+                                 y=dx[r].iloc[:,i],
+                                 visible=True if j==0 else False,
+                                 showlegend=True,
+                                 line=dict(color=cmap[i], width=linewidth[0]),))
+            
+            # Bottom plot - Relative to Long Bond
+            for i, n in enumerate(['full_sample', 'rolling']):
+                data.append(dict(type='scatter', name=n, xaxis='x2', yaxis='y2',
+                                 x=dx[r].index,
+                                 y=dx[r].iloc[:,2]-dx[r].iloc[:,i],
+                                 visible=True if j==0 else False,
+                                 fill='tozeroy', showlegend=False,
+                                 line=dict(color=cmap[i], width=linewidth[0])))
+            
+            # Append Button
+            vislist = []                             # Create a list for visible/hidden on args            
+            for x in range(len(dx.keys())):          # build list of True or Falses for all traces
+                if x == j: vislist.extend([True] * len(data))
+                else: vislist.extend([False] * len(data))
+            
+            # Actual button stuffs
+            button= dict(label= r, method = 'update',
+                         args = [{'visible': vislist},
+                                 {'title': 'Pokemon FV - {}'.format(r),}])
+    
+            layout['updatemenus'][0]['buttons'].append(button)    # append the button 
+            
+        if output:
+            return dict(data=data, layout=layout)
+        else:
+            self.plot = dict(data=data, layout=layout)
