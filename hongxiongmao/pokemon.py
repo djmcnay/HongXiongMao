2f89478bfcf1481754955096bcb495d46eb30200
djmcnay <djmcnay@gmail.com>
2019-06-21 16:07:43 +0100

    Create pokemon.py
    Moving Pokemon from badgermodels to own module

---
+++ hongxiongmao/pokemon.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+# Homemade Dependencies
+from hongxiongmao.utilities import tools, download
+
+# %%
+
+class pokemon(object):
+    
+    def __init__(self, region='US'):
+        self.region = region
+        return
+    
+    @property
+    def data(self): return self.__data
+    @data.setter
+    def data(self, data):              
+        if isinstance(data, pd.DataFrame):
+            data = data[::-1] if data.index[0] > data.index[-1] else data
+        else:
+            raise ValueError ('data takes pd.Dataframe object; {} sent'.format(type(data)))
+        self.__data = data
+    
+    # %% Ticker Dictionaries for Quandl Data
+    
+    quandl_us = {'US10YR':{'tickers':'USTREASURY/YIELD','fields':['10 YR']},
+                 'US02YR':{'tickers':'USTREASURY/YIELD','fields':['2 YR']},
+                 'UMich':{'tickers':'UMICH/SOC33','fields':['Median']},
+                 'ISM':{'tickers':'ISM/MAN_PMI', 'fields':[]},
+                 'REER':{'tickers':'BIS/EM_MRBUS','fields':[]}
+                 }
+
+    quandl_bund = {'DR10YR':{'tickers':'BUNDESBANK/BBK01_WT1010','fields':[]},
+                   'DR02YR':{'tickers':'BUNDESBANK/BBK01_WT0202','fields':[]},
+                   'CPI':{'tickers':'RATEINF/INFLATION_DEU','fields':[]},
+                   'OECD_CLI':{'tickers':'OECD/MEI_CLI_LOLITOAA_DEU_M', 'fields':[]},
+                   'REER':{'tickers':'BIS/EM_MRBXM','fields':[]}
+                   }
+
+    quandl_gilt = {'UK10YR':{'tickers':'BOE/IUDMNZC','fields':[]},
+                   'UK3MLIBOR':{'tickers':'ECB/FM_M_GB_GBP_RT_MM_GBP3MFSR__HSTA','fields':[]},
+                   'CPI':{'tickers':'RATEINF/INFLATION_GBR','fields':[]},
+                   'OECD_CLI':{'tickers':'OECD/MEI_CLI_LOLITONO_GBR_M', 'fields':[]},
+                   'REER':{'tickers':'BIS/EM_MRBGB','fields':[]}
+                   }
+
+    quandl_jgb = {'JP10YR':{'tickers':'MOFJ/INTEREST_RATE_JAPAN_10Y','fields':[]},
+                  'JP02YR':{'tickers':'MOFJ/INTEREST_RATE_JAPAN_2Y','fields':[]},
+                  'CPI':{'tickers':'RATEINF/INFLATION_JPN','fields':[]},
+                  'OECD_CLI':{'tickers':'OECD/MEI_CLI_LOLITOAA_JPN_M', 'fields':[]},
+                  'REER':{'tickers':'BIS/EM_MRBJP','fields':[]}
+                   }
+    
+    quandl_dicts = {'US':quandl_us, 'DR':quandl_bund, 'UK':quandl_gilt, 'JP':quandl_jgb}
+    
+    # %%
+    
+    def download_and_run(self, region=None, start_date='10y', rolling_window=36):
+        """
+        For single region pull Quandl data and run Pokemon model
+        """
+        region = self.region if region is None else region
+        self.pull_data_from_quandl(region=region, start_date=start_date)
+        self.model(window=rolling_window)
+        return
+    
+    # %% Quandl Data Import
+    def pull_data_from_quandl(self, region='US', start_date='5y', freq='monthly', output=False):
+        """
+        Download Pokemon Data from Quandl
+        """
+        
+        # Select relevant dictionary from regional input
+        if region in list(self.quandl_dicts.keys()):
+            tick_dic = self.quandl_dicts[region]
+        else:
+            raise ValueError('region {} doesn\'t currently have quandl tickers for Pokemon model'
+                             .format(region))
+        
+        # Iterate through dictionary & pull data
+        for i, v in enumerate(tick_dic.items()):
+            j = tick_dic[v[0]]
+            x = download().quandl_ts(tickers=j['tickers'], fields=j['fields'],
+                                  start_date=start_date, freq=freq)
+            
+            x.columns = [v[0]]    # Update column header with dictionary key
+            
+            # Merge new Dataframe output df
+            if i == 0: data = x
+            else: data = tools().df_merger(data, x)
+        
+        # output determines if we return data or set attribute
+        if output: return data
+        else: self.data = data
+    
+    # %%
+    def model(self, df=None, window=36, valiadate_data=True, output=False):
+        """
+        Pokemon Fair Value Model
+        """
+        
+        # Use internal data attribute if None is provided
+        df = self.data if df is None else df
+        
+        if valiadate_data is True:
+            df.fillna(method='ffill', inplace=True)   # Forward fill missing data
+            df = df[~df.isna().any(axis=1)]           # Each row complete
+        
+        # Dummy Dataframe(s)
+        guess = pd.DataFrame(data=df.iloc[:,0])
+        guess.columns = ['long_bond']
+        
+        coeff_vn = ['r2', 'intercept']
+        coeff_vn.extend(list(df.columns.values)[1:])
+        stats = pd.DataFrame(columns=coeff_vn)
+        
+        # Full Sample Period
+        y, X = df.iloc[:,0], df.iloc[:,1:]
+        lm = LinearRegression().fit(X,y)
+        guess['full_sample'] = lm.predict(X)
+        stats.loc['full_sample',['r2','intercept']]=lm.score(X, y),lm.intercept_
+        stats.loc['full_sample'].iloc[2:] = lm.coef_
+        
+        # Rolling Window
+        for i, v in enumerate(guess.index):
+            if i < window:
+                continue
+            
+            y = df.iloc[i-window:i,0]     # Dependant Var [long bond]
+            X = df.iloc[i-window:i,1:]    # Independant [short, inf, growth]
+            roll_lm = LinearRegression().fit(X,y)
+            guess.loc[v,'rolling'] = roll_lm.predict(X)[-1]
+            stats.loc[v,['r2', 'intercept']] = roll_lm.score(X, y), lm.intercept_
+            stats.loc[v].iloc[2:] = roll_lm.coef_ 
+            
+        # determine if we are returning an output or setting attributes
+        if output:
+            return guess, stats
+        else:
+            self.pokemodel = guess
+            self.stats_full = stats.loc['full_sample',:]
+            self.stats_roll = stats.iloc[1:,:]
+
+# %%
+
+class pokemon_go(object):
+    """
+    Pokemon Go
+    
+    Aggregation class for Pokemon model, will run several Pokemon models at once
+    and store the data internally. Option also available to generate Plotly plot.
+    Name is a nod to the iPhone Game Will & I were playing during first build.
+    
+    """
+    
+    def __init__(self, regions=['US']):
+        
+        self.regions = regions
+        self.data, self.stats, self.pokemodel = [dict.fromkeys(regions)] * 3
+        #self.stats = dict.fromkeys(regions)
+        # self.pokemodel = dict.fromkeys(regions)
+
+    def download_and_run(self, output=False):
+        """
+        
+        """
+        
+        for r in self.regions:        # iterate through each region
+            x = pokemon(region=r)     # setup pokemon class for r
+            x.download_and_run()      # download from Quandl & run
+            
+            # Store data in go dictionaries
+            self.data[r] = x.data
+            self.stats[r] = {'full':x.stats_full, 'rolling':x.stats_roll}
+            self.pokemodel[r] = x.pokemodel
+        
+        if output:
+            return self.pokemodel, self.stats, self.data
+        
+    def plotlyplot():
+        
+        return fig
+            
+# %%
+
+p = pokemon_go().download_and_run(True)
+
