ff8ed2128e5a040dbdec7c1b5864330d61332734
djmcnay <djmcnay@gmail.com>
2019-07-02 21:47:06 +0100

    Annoyance

--- hongxiongmao/_testspace.py
+++ hongxiongmao/_testspace.py
@@ -13,17 +13,8 @@ from download import quandl_hxm
 
 # %%
 
-#dl = quandl_hxm()
-#cli = dl.from_tickerdict('oecd_cli', start_date='-5y')
-
-# %%
-
-#cli = cli - 100
-#cli_dm = cli.loc[:,['US', 'UK', 'EURO', 'JAPAN', 'AUS', 'CANADA', 'KOREA', 'OECD']]
-#cli_groups_dm = dict(ALL=list(cli_dm.columns), Major=['US', 'EURO', 'UK', 'JAPAN'], Other=['AUS', 'CANADA', 'KOREA', 'OECD'])
-
 dl = quandl_hxm()
-cli = dl.from_tickerdict('oecd_cli', start_date='-20y') - 100
+#cli = dl.from_tickerdict('oecd_cli', start_date='-5y') - 100
 
 # %%
 
@@ -32,33 +23,8 @@ cli_dm = cli.loc[:,['US', 'UK', 'EURO', 'JAPAN', 'AUS', 'CANADA', 'KOREA', 'OECD
 cli_groups_dm = dict(ALL=list(cli_dm.columns), Major=['US', 'EURO', 'UK', 'JAPAN'],
                      Other=['AUS', 'CANADA', 'KOREA', 'OECD'])
 
-fig = overplot.scatter_from_dataframe(cli_dm, groups=cli_groups_dm,
-                                      title='OECD Normalised CLI - Developed Markets',
-                                      ytitle='Normalised CLI (minus 100)',
-                                      button_position=[1.1, 0.25])
+fig = overplot.swirlygram(cli.iloc[:,0:5], title='OECD Normalised CLI Swirlygram',
+                 yaxis={'title':'some shit'},)
 
 py.plot(fig)
-# %%
 
-cli_em = cli.loc[:,['CHINA', 'MEXICO', 'RUSSIA', 'INDIA', 'BRAZIL', 'TURKEY', 'ZAR', 'ASIA5']]
-cli_groups_em = dict(ALL=list(cli_em.columns), BRIC=['CHINA', 'BRAZIL', 'RUSSIA', 'INDIA'],
-                     Asia=['CHINA', 'INDIA', 'ASIA4'], Latam=['MEXICO', 'BRAZIL'], CEEMEA=['RUSSIA', 'TURKEY', 'ZAR'])#
-
-fig = overplot.scatter_from_dataframe(cli_em, groups=cli_groups_em,
-                                      title='OECD Normalised CLI - Emerging Markets',
-                                      ytitle='Normalised CLI (minus 100)',
-                                      button_position=[0, -0.1],
-                                      button_direction = 'right')
-py.plot(fig)
-#py.plot(fig, filename='PlotlyHTMLexJS/cli_em.html', auto_open=False, include_plotlyjs='cdn',include_mathjax='cdn')
-
-# %%
-
-
-#fig = overplot.scatter_from_dataframe(cli_dm, groups=cli_groups_dm,
-#                                      title='OECD Normalised CLI - Developed Markets',
-#                                      ytitle='Normalised CLI (minus 100)',
-#                                      button_position = [0,-0.1,0,0,0,0],
-#                                      button_direction = 'right')
-
-#py.plot(fig)
