1d985dc3423a38e411cba3392b917201418c044f
djmcnay <djmcnay@gmail.com>
2019-07-02 21:14:23 +0100

    Update overplot.py
    Adding static swirlygram

--- hongxiongmao/overplot.py
+++ hongxiongmao/overplot.py
@@ -14,19 +14,20 @@ import numpy as np
 import pandas as pd
 import copy
 
-COLOURMAP = {0:'purple', 1:'turquoise', 2:'grey', 3:'black', 4:'lime', 5:'blue', 6:'orange', 7:'red'}
+COLOURMAP = {0:'purple', 1:'turquoise', 2:'grey', 3:'black', 4:'green',
+             5:'blue', 6:'crimson', 7:'orange', 8:'mediumvioletred'}
 
 DEFAULT_LAYOUT = dict(title='Title',
                       font={'family':'Courier New', 'size':12},
                       showlegend=True,
                       legend={'orientation':'v'},
                       margin = {'l':75, 'r':50, 'b':50, 't':50},
-                      xaxis1= {'anchor': 'y1', 'title': '',},
-                      yaxis1= {'anchor': 'x1', 'title': '', 'hoverformat':'.2f', 'tickformat':'.1f'},
+                      xaxis= {'anchor': 'y1', 'title': '',},
+                      yaxis= {'anchor': 'x1', 'title': '', 'hoverformat':'.2f', 'tickformat':'.1f'},
                       updatemenus= [dict(type='buttons',
                                          active=-1, showactive = True,
-                                         direction='right',
-                                         y=0.25, x=1.1,
+                                         direction='down',
+                                         y=0.5, x=1.1,
                                          pad = {'l':0, 'r':0, 't':0, 'b':0},
                                          buttons=[])],
                       annotations=[],)
@@ -56,9 +57,8 @@ def _update_layout(layout, **kwargs):
         elif k in layout and isinstance(v, dict):
             # if v is a dict, either replace or append to dict
             for k1, v1, in v.items():
-                if v1 in layout[k][k1]:
-                    layout[k][k1] = v1
-    
+                layout[k][k1] = v1
+                    
     ### Second Level => bespoke kwarg inputs
     
     # Button position update
@@ -92,8 +92,8 @@ def scatter_from_dataframe(df, **kwargs):
     """
     
     # Bring in Defaults from Global
-    cmap=COLOURMAP
-    layout=copy.deepcopy(DEFAULT_LAYOUT)
+    cmap= COLOURMAP.copy()
+    layout= copy.deepcopy(DEFAULT_LAYOUT)  # deepcopy important bedded dicts
     
     ## Basic Layout Dictionary
     # Needs to come first or buttons have nowhere to append too
@@ -120,3 +120,92 @@ def scatter_from_dataframe(df, **kwargs):
     fig = dict(data=data, layout=layout)
 
     return fig
+
+# %% Swirlygrams
+    
+def swirlygram(df, n=3, trail=18, quadrants=True, **kwargs):
+    """
+    Static Swirlygram with multiple traces
+    
+    Shows the absolute level of (for example) a composite leading indicator and 
+    the change in the indictor over n periods, with a for previous observations.
+    
+    This version is STATIC and only shows the most recent Swirlygram for each
+    column in the input dataframe, with buttons to shrink the number of plots 
+    shown; seperate function being built for an animated timeseries Swirlygram.
+    
+    INPUTS:
+        df - dataframe with each column being a seperate series
+        n - periodic change (default == 3)
+             Currently this is the absolute shift, but will add percentage
+        trail - length of history to show in comet tail (default==18)
+        quadrants - True (default) | False and adds coloured quadrants around origin
+        
+    
+    DEVELOPMENTS:
+        * qudrant function to allow us to move origin about
+        * Percentage change or other x-axis stuff
+            
+    """
+    
+    ### Default Setup
+    cmap=COLOURMAP.copy()
+    layout=copy.deepcopy(DEFAULT_LAYOUT)
+    layout=_update_layout(layout, **kwargs)
+    
+    ### Manipulate Data
+    df = df.iloc[-(trail+n):,:]               # subset input df to required length
+    x = (df - df.shift(n)).iloc[-trail:,:]    # x-axis given as change
+    y = df.iloc[-trail:,:]                      # y-axis suset to length of tail
+    
+    ### Append Data
+    data=[]
+    for i, c in enumerate(df.columns):
+
+        # LINE
+        data.append(dict(type='scatter', name=c, mode='lines+markers', showlegend=True,
+                    x=x.loc[:,c], y=y.loc[:,c],
+                    line=dict(color=cmap[i], width=1),))
+            
+        # MARKER for most recent observation
+        data.append(dict(type='scatter', mode='markers', showlegend=False, 
+                    x=[x.iloc[-1,:].loc[c]], y=[y.iloc[-1,:].loc[c]],
+                    marker=dict(symbol='diamond', color=cmap[i], size=10,
+                               line={'color':'black', 'width':1}),))
+    
+        ## Buttons
+        if i == 0: l = len(data)                   # find no traces on 1st pass
+        visible = [False] * l * len(df.columns)    # List of False = Total No of Traces
+        visible[(i*l):(i*l)+l] = [True] * l        # Make traces of ith pass visible
+        
+        button= dict(label=c, method='update', args=[{'visible': visible},])
+        layout['updatemenus'][0]['buttons'].append(button)     # append the button 
+    
+    ### Additional Layout Changes
+    
+    # Symmetrical Plot around zero
+    absmax = lambda df, x=1: np.max([df.abs().max().max(), x]) * 1.05    
+    layout['xaxis']['range'] = [-absmax(x), absmax(x)]
+    layout['yaxis']['range'] = [-absmax(y), absmax(y)]
+    
+    # Rectangles of colour for each quadrant
+    if quadrants:
+        shapes = [{'type':'rect', # Top Right
+                   'xref':'x', 'x0':0, 'x1':100,
+                   'yref':'y', 'y0':0, 'y1':100,
+                   'line':{'width': 0,},'fillcolor':'green', 'opacity': 0.1,},    
+                  {'type':'rect', # Bottom Left
+                   'xref':'x', 'x0':-100, 'x1':0, 
+                   'yref':'y', 'y0':-100, 'y1':0,
+                   'line':{'width': 0,}, 'fillcolor':'red', 'opacity': 0.1,},    
+                  {'type':'rect', # Top Left
+                   'xref':'x', 'x0':-100, 'x1':0,
+                   'yref':'y', 'y0':0, 'y1':100, 
+                   'line':{'width': 0,}, 'fillcolor':'blue', 'opacity': 0.1,},
+                  {'type': 'rect', # Bottom Right
+                   'xref':'x', 'x0':0, 'x1':100,
+                   'yref':'y', 'y0':-100, 'y1':0,
+                   'line':{'width': 0,}, 'fillcolor':'orange', 'opacity': 0.1,},]
+        layout['shapes'] = shapes
+    
+    return dict(data=data, layout=layout)
