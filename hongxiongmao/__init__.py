293fe47fa3008a80065ba384e04276cd3d9a6d52
djmcnay <djmcnay@gmail.com>
2019-07-20 16:00:11 +0100

    Update __init__.py

--- hongxiongmao/__init__.py
+++ hongxiongmao/__init__.py
@@ -4,6 +4,13 @@
 Created on Sat Jun 15 16:56:31 2019
 
 @author: David
+
+
+
 """
 
+# Useful Medium post on Python importing within __init__.py
+# https://towardsdatascience.com/whats-init-for-me-d70a312da583
+from hongxiongmao import pokemon
 import hongxiongmao.download
+import utilities
