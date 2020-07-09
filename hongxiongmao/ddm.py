29b4fe01a19464760c0fa618e3c0422606d61250
djmcnay <39102979+djmcnay@users.noreply.github.com>
2020-07-09 16:13:35 +0200

    Update ddm.py

--- hongxiongmao/ddm.py
+++ hongxiongmao/ddm.py
@@ -2,14 +2,49 @@
 """
 Dividend Discount Model(s)
 
+Series of functions for solving dividend discount model problems. Primarily
+
+- dividend_discount_model_irr() is a sovler for given px, divident stream & G
+- multi_stage_irr() is a 3-stage model which estimates g = ROE * (1-PO)
+
+
 """
 
-
-# %%
+# %% REQUIRED PACKAGES
 
 import pandas as pd
 import numpy as np
 from scipy.optimize import minimize_scalar
+
+# %% DDM Solver Functions - given dividend stream
+# Build a dividend stream using another function - solve it here
+
+def dividend_discount_model_irr(px, vDividend, G):
+    """ IRR finder for Dividend Discount Model in SciPy
+    
+    Uses the scipy.optimise minimize_scalar optimisation function
+        minimize_scalar(f, args=(d, px, G)).x
+        
+    Where d - vector of dividend stream from d0 to dTerminal
+          px - current index level associated with dividend stream
+          G = perpetual growth rate
+    """
+    
+    return minimize_scalar(_solver_ddm_irr, args=(px, vDividend, G)).x
+
+def _solver_ddm_irr(x, px, d, G):
+    """ solver function for dividend_discount_model_irr"""
+    pv = 0     # Set present value of div stream to 0 to t0
+    for i, v in enumerate(d):
+        
+        if i == 0:              # skip d0 which isn't included in DDM calc
+            continue
+        elif i < len(d) - 1:    # len(d)-1 == terminal year
+            pv += v / ((1 + x) ** i)
+        else:
+            pv += (v / ((1 + x) ** i)) * ( 1 / (x - G))
+            
+    return np.abs(px - pv)    # minimise in optimiser
 
 # %% Equity Sustainable Returns Model
 
@@ -22,13 +57,15 @@ def multi_stage_irr(dv,
         1. Convergence to trend
         2. Time in trend
         3. Perpetual growth
+
+    growth with growth estimated as g = ROE * (1 - Payout_Ratio)
     
     INPUTS:
         dv = data-vector which can either be a list or a pd.Series; must contain: 
              ['PX', 'D0', 'ROE', 'PO', 'ROE_trend', 'PO_trend', 'G']
         trend_start = int of year (default=10) entering trend growth
         terminal = int or year (default=21) entering perpetual growth phase
-
+    
     """
     
     if isinstance(dv, list):
@@ -55,38 +92,18 @@ def multi_stage_irr(dv,
     
     ddm.loc[ddm.index[-1], 'g'] = dv['G']    # set Terminal Growth Rate in table
     
-    # Scipy optimisation
-    # solver function is _opt_irr
-    # Required args are Vector Dividends, current PX and Terminal G
-    res = minimize_scalar(_opt_irr, args=(ddm.D, ddm.PX[0], ddm.g.iloc[-1]))
+    # solve for IRR using homemade function
+    r = dividend_discount_model_irr(dv.PX, ddm.D, dv.G)
     
-    return ddm, res
-    
-# Optimisation Function for IRR
-def _opt_irr(x, d, px, G):
-    """ IRR function for Multi-Stage Dividend Discount Model in Scipy
-    
-    Uses the scipy.optimise minimize_scalar optimisation function
-        minimize_scalar(f, args=(d, px, G)).x
-        
-    Where d - vector of dividend stream from d0 to dTerminal
-          px - current index level associated with dividend stream
-          G = perpetual growth rate """
-    
-    pv1 = 0        # Present Value of "phases"
-    n = len(d)-1   # Year of Terminal Growth (-1 removes t0 from height)
-    
-    # Geometric sum of PV from year 1 to terminal
-    d1 = d[1:-1].values
-    for i, v in enumerate(d1):
-        pv1 += d1[i] / ((1 + x) ** i) 
-    
-    # Calculate PV of Terminal Value
-    pvT = (d[n] / ((1 + x) ** n)) * (1 / (x - G))
-    
-    return np.abs(pv1 + pvT - px)    # minimise in optimiser
+    # Show present value in table    
+    for i, v in enumerate(ddm.index):
+        if i == 0:
+            continue
+        elif v < terminal:
+            ddm.loc[v,'PX'] = -1 * ddm.loc[v,'D']/((1+r)**i)
+        else:
+            ddm.loc[v,'PX'] = (ddm.loc[v,'D'] /((1+r)**i)) * -1 * (1 / (r - ddm.g.iloc[-1]))
+            
+    return ddm, r
 
 # %%
-dv = [11.9, 0.237, 0.13, 0.425, 0.14, 0.35, 0.01]
-x, y = multi_stage_irr(dv)
-
